e2b83176e1d93cb5ccc44589aa34f27da45c0fbf
KashAlOt4SV <shaya.ashot@mail.ru>
2022-04-17 14:36:49 +0300

    make it better

--- data/db_session.py
+++ data/db_session.py
@@ -17,17 +17,18 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = 'sqlite:///' + db_file.strip()
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
 
+    # noinspection PyUnresolvedReferences
     from . import __all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
 
 
-def create_session() -> Session:
+def create_session() -> None:
     global __factory
-    return __factory()
+    return __factory
